b37ca7fa5ead1c5885c060eafb559424511af850
Oranda <codalreef@gmail.com>
2021-08-18 15:29:13 -0400

    remove link path for now

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_native_1 = require("react-native");
 exports.saveToHome = (params) => {
-    const { link, manifest, server = "https://codalreef.github.io/Homeward-Server/link" } = params;
+    const { link, manifest, server = "https://codalreef.github.io/Homeward-Server" } = params;
     const queryParams = `?
     manifest=${encodeURIComponent(JSON.stringify(manifest))}&
     link=${encodeURIComponent(link)}
